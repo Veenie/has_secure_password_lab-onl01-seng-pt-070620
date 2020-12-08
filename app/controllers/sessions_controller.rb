5f79234f4c83a0e60d3db11e11869c3fd1fda495
Raveen K Sharma <raveen08@gmail.com>
2020-12-08 20:17:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 
     @user = user
 
-    redirect_to controller: 'welcome', action: 'home'
+    redirect_to controller: 'user', action: 'home'
   end
 
   def destroy
