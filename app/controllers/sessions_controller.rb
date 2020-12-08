2de15074bd561e7af242b4ca330ac67c567f0c9c
Raveen K Sharma <raveen08@gmail.com>
2020-12-08 20:10:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(name: params[:user][:name])
+    user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.save
     session[:user_id] = @user.id
   end
