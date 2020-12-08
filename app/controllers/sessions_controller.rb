1c19c4b113e86ce91bf0139b24896ac3192a05fd
Raveen K Sharma <raveen08@gmail.com>
2020-12-08 20:11:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:user][:name])
+    
     return redirect_to(controller: 'sessions', action: 'new') unless user.save
     session[:user_id] = user.id
   end
